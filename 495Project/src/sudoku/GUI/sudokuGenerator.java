b9c1a09f46bcb8f46a1f3b38c1ab1a97162529df
Brxaan <77035968+Brxaan@users.noreply.github.com>
2022-04-24 15:55:06 -0700

    Update sudokuGenerator.java

--- 495Project/src/sudoku/GUI/sudokuGenerator.java
+++ 495Project/src/sudoku/GUI/sudokuGenerator.java
@@ -1,157 +1,186 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+/**
+ * Brian Jefferson
+ * 
+ * 04/22/2022
+ * 
+ * Sudoku Generator; this class is used to randomly generate a sudoku using square root. Anywhere there is a zero 
+ * that is represented as a blank space. 
+ * 
+ * 04/24/2022 ---->  
+ * 					Modified java class to randomly generate new values every run
+ * 
+ *
+ */
 
 public class sudokuGenerator {
+	
+    int[][] mat;
+    int column; // Number of columns/rows.
+    int squareRoot; // Square root of column
+    int missingDigits; // Number Of missing digits
 
-    private Table sudokuTable = new Table();
+    // Constructor
+    sudokuGenerator(int column, int missingDigits) {
+    	
+        this.column = column;
+        this.missingDigits = missingDigits;
 
-    private JFrame frame = new JFrame("Sudoku Game");
-    private JTextField textField[][] = new JTextField[9][9];
-    private GridPanel gridPanel = new GridPanel(new GridLayout(9, 9, 1, 1));
+        Double sqrt = Math.sqrt(column);
+        squareRoot = sqrt.intValue();
 
-    // constructor //
-    void GameGUI() {
-        for (int y = 0; y < 9; y++) {
-            for (int x = 0; x < 9; x++) {
-                textField[x][y] = new JTextField();
-                textField[x][y].setForeground(Color.YELLOW);
-                gridPanel.add(textField[x][y]);
-            }
-        }
+        mat = new int[column][column];
     }
 
-    boolean checkText() {
-        for (int y = 0; y < 9; y++) {
-            for (int x = 0; x < 9; x++) {
-                if (!textField[x][y].getText().equals("")) {
-                    try {
-                        int digit = Integer.parseInt(textField[x][y].getText());
-                        if (digit <= 0 || digit >= 10)
-                            return false;
-                    } catch (NumberFormatException e) {
-                        return false;
-                    }
-                }
-            }
-        }
+    public static void main(String[] args) {
+    	
+        int column = 9, missingDigits = 20;
+        sudokuGenerator sudoku = new sudokuGenerator(column, missingDigits);
+        sudoku.fillValues();
+        sudoku.getBoard();
+    }
+
+    // Sudoku Generator
+    public void fillValues() {
+    	
+        // Fill the diagonal
+        fillDiagonal();
+
+        // Fill remaining spaces
+        fillRemaining(0, squareRoot);
+
+        // Remove missing digits 
+        removeDigits();
+    }
+
+    // Fill the diagonal squareRoot number of squareRoot x squareRoot matrices
+    void fillDiagonal() {
+
+        for (int k = 0; k < column; k = k + squareRoot)
+
+            // for diagonal box, start coordinates->k==j
+            fillBox(k, k);
+    }
+
+    // Returns false if given 3 x 3 block contains num.
+    boolean unUsedBox(int rowStart, int colStart, int num) {
+        for (int k = 0; k < squareRoot; k++)
+            for (int j = 0; j < squareRoot; j++)
+                if (mat[rowStart + k][colStart + j] == num)
+                    return false;
+
         return true;
     }
 
-    public void GUIToSudoku() {
-        for (int y = 0; y < 9; y++) {
-            for (int x = 0; x < 9; x++) {
-                if (!textField[x][y].getText().equals("")) {
-                    sudokuTable.getDigit(x, y).setAnswer(Integer.parseInt(textField[x][y].getText()));
-                    sudokuTable.getDigit(x, y).setSafe(true); // set starting digits to true
-                    textField[x][y].setForeground(Color.YELLOW); // color starting digits yellow
-                } else {
-                    sudokuTable.getDigit(x, y).setAnswer(0); // clear any blank table digits by setting them to 0
-                    textField[x][y].setForeground(Color.BLACK); // color blanks to black
+    // Fill a 3 x 3 matrix.
+    void fillBox(int row, int col) {
+        int num;
+        for (int k = 0; k < squareRoot; k++) {
+            for (int j = 0; j < squareRoot; j++) {
+                do {
+                    num = randomGen(column);
                 }
+                while (!unUsedBox(row, col, num));
+
+                mat[row + k][col + j] = num;
             }
         }
     }
 
-    public void sudokuTable() {
-        for (int y = 0; y < 9; y++) {
-            for (int x = 0; x < 9; x++) {
-                if (sudokuTable.getDigit(x, y).isSolved())
-                    textField[x][y].setText(String.valueOf(sudokuTable.getDigit(x, y).getAnswer()));
+    // Random generator
+    int randomGen(int num) {
+        return (int) Math.floor((Math.random() * num + 1));
+    }
+
+    // Check if safe to put in cell
+    boolean CheckIfSafe(int k, int j, int num) {
+        return (unUsedRow(k, num) &&
+                unUsedCol(j, num) &&
+                unUsedBox(k - k % squareRoot, j - j % squareRoot, num));
+    }
+
+    // check in the row for existence
+    boolean unUsedRow(int k, int num) {
+        for (int j = 0; j < column; j++)
+            if (mat[k][j] == num)
+                return false;
+        return true;
+    }
+
+    // check in the row for existence
+    boolean unUsedCol(int j, int num) {
+        for (int k = 0; k < column; k++)
+            if (mat[k][j] == num)
+                return false;
+        return true;
+    }
+
+    // A recursive function to fill remaining
+    // matrix
+    boolean fillRemaining(int k, int j) {
+        //  System.out.println(k+" "+j);
+        if (j >= column && k < column - 1) {
+            k = k + 1;
+            j = 0;
+        }
+        if (k >= column && j >= column)
+            return true;
+
+        if (k < squareRoot) {
+            if (j < squareRoot)
+                j = squareRoot;
+        } else if (k < column - squareRoot) {
+            if (j == (k / squareRoot) * squareRoot)
+                j = j + squareRoot;
+        } else {
+            if (j == column - squareRoot) {
+                k = k + 1;
+                j = 0;
+                if (k >= column)
+                    return true;
+            }
+        }
+
+        for (int num = 1; num <= column; num++) {
+            if (CheckIfSafe(k, j, num)) {
+                mat[k][j] = num;
+                if (fillRemaining(k, j + 1))
+                    return true;
+
+                mat[k][j] = 0;
+            }
+        }
+        return false;
+    }
+
+    // Remove missing digits
+    public void removeDigits() {
+        int count = missingDigits;
+        while (count != 0) {
+            int cell = randomGen(column * column) - 1;
+
+            int k = (cell / column);
+            int j = cell % 9;
+            if (j != 0)
+                j = j - 1;
+
+            // System.out.println(k+" "+j);
+            if (mat[k][j] != 0) {
+                count--;
+                mat[k][j] = 0;
             }
         }
     }
 
-    public void clearGrid() {
-        for (int y = 0; y < 9; y++) {
-            for (int x = 0; x < 9; x++) {
-                textField[x][y].setText("");
-                textField[x][y].setForeground(Color.YELLOW);
-            }
+    // Print sudoku
+    public int[][] getBoard() {
+        for (int k = 0; k < column; k++) {
+            for (int j = 0; j < column; j++)
+                System.out.print(mat[k][j] + " ");
+            System.out.println();
         }
+        System.out.println();
+		return mat;
     }
 
-    public void createGUI() {
-        JPanel PANEL = new JPanel(new GridBagLayout());
-        GridBagConstraints gridBagConstraints = new GridBagConstraints();
 
-        gridBagConstraints.weighty = 1;
-        gridBagConstraints.weightx = 1;
-        gridBagConstraints.anchor = GridBagConstraints.NORTH;
-
-        //add label to top of main panel
-        JLabel topLabel = new JLabel("Sudoku Game", SwingConstants.CENTER);
-        topLabel.setOpaque(true);
-        topLabel.setBackground(Color.BLACK);
-        topLabel.setForeground(Color.WHITE);
-        topLabel.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridwidth = 3;
-        gridBagConstraints.weighty = 0.05;
-        gridBagConstraints.fill = GridBagConstraints.BOTH;
-        PANEL.add(topLabel, gridBagConstraints);
-
-
-        //add grid panel
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.weighty = 1;
-        gridBagConstraints.fill = GridBagConstraints.BOTH;
-        gridBagConstraints.anchor = GridBagConstraints.NORTH;
-        PANEL.add(gridPanel, gridBagConstraints);
-
-        //set these gridBagConstraints for all bottom buttons
-        gridBagConstraints.anchor = GridBagConstraints.SOUTH;
-        gridBagConstraints.weighty = 0.1;
-
-        JButton generateButton = new JButton("Generate");
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.gridwidth = 1;
-        gridBagConstraints.anchor = GridBagConstraints.SOUTH;
-        gridBagConstraints.ipadx = 40;
-        PANEL.add(generateButton, gridBagConstraints);
-
-        JButton clearButton = new JButton("Clear Table");
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.gridwidth = 1;
-        gridBagConstraints.ipadx = 0;
-        PANEL.add(clearButton, gridBagConstraints);
-
-        //create frame and add main panel
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-        frame.getContentPane().add(PANEL); //add main panel to frame
-        frame.setLocationRelativeTo(null);
-        frame.setMinimumSize(new Dimension(400, 400));
-        frame.setVisible(true);
-    }
-
-    public class GridPanel extends JPanel{
-		private static final long serialVersionUID = 1L;
-
-		GridPanel(GridLayout layout){
-			super(layout);
-		}
-    }
-    public class SudokuStart {
-        public static void main(String args[]) {
-            GameGUI gui = new GameGUI();
-            gui.createGUI();
-        }
-    }
+}
